8cad4346a1c8f8fabb9271556b4c708cd2a8d1da
Jeremy Whitney <bearwhitness@gmail.com>
2024-09-26 13:46:49 -0500

    Changed the Rating to a dyanmic import, where SSR = false

--- components/rating/container.js
+++ components/rating/container.js
@@ -1,12 +1,11 @@
-import { RatingCard } from './card'
-import RatingForm from './form'
+import { RatingCard } from "./card";
+import RatingForm from "./form";
 
 export function RatingsContainer({ ratingCount, saveRating }) {
   return (
     <div className="tile is-parent is-12 is-vertical container">
       <RatingForm saveRating={saveRating} />
-      {/* Display the rating count instead of mapping through ratings */}
-      <p>{`This product has ${ratingCount} ratings.`}</p>
+      {/* RatingCard here ? */}
     </div>
-  )
+  );
 }
