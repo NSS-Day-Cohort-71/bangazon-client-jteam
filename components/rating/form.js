81ba91004609e918b41c7bf3795ff6fc68661f0e
Jeremy Whitney <bearwhitness@gmail.com>
2024-10-02 13:05:41 -0500

    Resolved 404 error and subsequent missing product ID and rating submission errors.

--- components/rating/form.js
+++ components/rating/form.js
@@ -12,19 +12,39 @@ export default function RatingForm({ saveRating }) {
     }
   );
 
+  const handleRating = (rate) => {
+    setRating(rate);
+  };
+
   const submitRating = () => {
-    const outOf5 = rating / 20;
-    saveRating({
+    const outOf5 = rating;
+
+    if (rating === 0) {
+      alert("Please select a rating");
+      return;
+    }
+
+    const ratingData = {
       score: outOf5,
-      review: comment,
-    });
+      rating_text: comment,
+    };
+
+    saveRating(ratingData);
+
+    // Reset form
+    setRating(0);
+    setComment("");
   };
 
   return (
-    <div className="tile is-child ">
+    <div className="tile is-child">
       <article className="media box">
         <figure className="media-left">
-          <Rating onClick={setRating} ratingValue={rating} />
+          <Rating
+            onClick={handleRating}
+            ratingValue={rating}
+            initialValue={0}
+          />
         </figure>
         <div className="media-content">
           <div className="field">
