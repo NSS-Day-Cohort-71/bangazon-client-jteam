27d6e61796fc1a7d41914b0dc3f72e2374aa13a2
James Campbell <jamesrcampbell.obb@gmail.com>
2024-10-04 11:08:10 -0500

    added feature for liking and unliking a product

--- components/product/detail.js
+++ components/product/detail.js
@@ -5,37 +5,34 @@ import Modal from "../modal"
 import { Input } from "../form-elements"
 
 export function Detail({ product, like, unlike }) {
-  const router = useRouter()
-  const usernameEl = useRef()
-  const [showModal, setShowModal] = useState(false)
-  const [showError, setShowError] = useState(false)
-
+  const router = useRouter();
+  const usernameEl = useRef();
+  const [showModal, setShowModal] = useState(false);
+  const [showError, setShowError] = useState(false);
 
   const addToCart = () => {
     addProductToOrder(product.id).then(() => {
-      router.push('/cart')
-    })
-  }
+      router.push("/cart");
+    });
+  };
 
   const recommendProductEvent = () => {
     recommendProduct(product.id, usernameEl.current.value).then((res) => {
       if (res) {
-        setShowError(true)
+        setShowError(true);
       } else {
-        setShowModal(false)
-        setShowError(false)
-        usernameEl.current.value = ""
+        setShowModal(false);
+        setShowError(false);
+        usernameEl.current.value = "";
       }
-    })
-  }
+    });
+  };
 
   return (
     <>
       <Modal setShowModal={setShowModal} showModal={showModal} title="Recommend this product to a user">
         <Input id="username" label="Enter a username" refEl={usernameEl}>
-          {
-            showError ? <p className="help is-danger">This user doesn't exist</p> : <></>
-          }
+          {showError ? <p className="help is-danger">This user doesn't exist</p> : <></>}
         </Input>
         <>
           <button className="button is-success" onClick={recommendProductEvent}>Recommend Product</button>
@@ -50,7 +47,7 @@ export function Detail({ product, like, unlike }) {
             </figure>
           </article>
         </div>
-        <div className="tile is-parent is-vertical ">
+        <div className="tile is-parent is-vertical">
           <article className="tile is-child">
             <h1 className="title">{product.name} - ${product.price}</h1>
             <p className="subtitle">{product.store?.name}</p>
@@ -63,27 +60,27 @@ export function Detail({ product, like, unlike }) {
                 <button className="button is-primary" onClick={addToCart}>Add to Cart</button>
               </p>
               <p className="control">
-                <button
-                  className="button is-danger is-outlined"
-                  onClick={() => setShowModal(true)}
-                >Recommend this Product</button>
+                <button className="button is-danger is-outlined" onClick={() => setShowModal(true)}>
+                  Recommend this Product
+                </button>
               </p>
               <p className="control">
                 {
-                  product.is_liked ?
+                  product.is_liked ? (
                     <button className="button is-link is-outlined" onClick={unlike}>
                       <span className="icon is-small">
                         <i className="fas fa-heart-broken"></i>
                       </span>
                       <span>Unlike Product</span>
                     </button>
-                    :
+                  ) : (
                     <button className="button is-link is-outlined" onClick={like}>
                       <span className="icon is-small">
                         <i className="fas fa-heart"></i>
                       </span>
                       <span>Like Product</span>
                     </button>
+                  )
                 }
               </p>
             </div>
@@ -91,5 +88,5 @@ export function Detail({ product, like, unlike }) {
         </div>
       </div>
     </>
-  )
+  );
 }
