5ed8496ae99ed2879612d3565e2e33301bdaa479
Jana Ismail <jana.ismail.nss@gmail.com>
2024-10-07 11:02:18 -0500

    Feature to favorite store working commit

--- data/stores.js
+++ data/stores.js
@@ -39,22 +39,23 @@ export function editStore(store) {
 }
 
 export function favoriteStore(storeId) {
-  return fetchWithoutResponse(`profile`, {
+  return fetchWithoutResponse(`profile/favoritesellers`, {
     method: 'POST',
     headers: {
       Authorization: `Token ${localStorage.getItem('token')}`,
       'Content-Type': 'application/json'
     },
-    body: JSON.stringify({"store_id": 3})
+    body: JSON.stringify({"store_id": storeId})
   })
 }
 
 export function unfavoriteStore(storeId) {
-  return fetchWithoutResponse(`stores/${storeId}/unfavorite`, {
+  return fetchWithoutResponse(`profile/favoritesellers`, {
     method: 'DELETE',
     headers: {
       Authorization: `Token ${localStorage.getItem('token')}`,
       'Content-Type': 'application/json'
     },
+    body: JSON.stringify({"store_id": storeId})
   })
 }
